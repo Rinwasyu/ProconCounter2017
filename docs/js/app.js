6d6318ccf6bf6ae2281d52c63efbeaa92fdc3a4c
kadoshita <kadoshita.inct@gmail.com>
2016-12-25 00:44:04 +0900

    add count up animetion

--- docs/js/app.js
+++ docs/js/app.js
@@ -1,4 +1,9 @@
-var countText=document.getElementById('count-text');
+var daysText=document.getElementById('days-text');
+var hourText=document.getElementById('hour-text');
+var minText=document.getElementById('min-text');
+var secText=document.getElementById('sec-text');
+
+var isAnime=false;
 
 setInterval(function () {
 	var date=new Date();
@@ -9,14 +14,59 @@ setInterval(function () {
 
 	day=~~(milliSec/(1000*60*60*24));
 	milliSec-=day*1000*60*60*24;
+	if(day<10){
+		day='00'+day;
+	}else if (day<100) {
+		day='0'+day;
+	}
 
 	hour=~~(milliSec/(1000*60*60));
 	milliSec-=hour*1000*60*60;
+	if(hour<10){
+		hour='0'+hour;
+	}
 
 	min=~~(milliSec/(1000*60));
 	milliSec-=min*1000*60;
+	if(min<10){
+		min='0'+min;
+	}
 
 	sec=~~(milliSec/1000);
+	if(sec<10){
+		sec='0'+sec;
+	}
 
-	countText.innerHTML=day+'日'+hour+'時間'+min+'分'+sec+'秒';
-},1000)
+	if (!isAnime) {
+		animeNumber(daysText,day);
+		animeNumber(hourText,hour);
+		animeNumber(minText,min);
+		animeNumber(secText,sec);
+		isAnime=true;
+	} else {
+		appendNumber(daysText,day);
+		appendNumber(hourText,hour);
+		appendNumber(minText,min);
+		appendNumber(secText,sec);
+	}
+},1000);
+
+function animeNumber (element,count) {
+	var i=0;
+	var interval=10;
+	if(count>100){
+		interval=1;
+		i=90;
+	}
+	var timer=setInterval(function () {
+		element.innerHTML=i;
+		if(i>=count){
+			clearInterval(timer);
+		}
+		i++;
+	},interval);
+}
+
+function appendNumber (element,number) {
+	element.innerHTML=number;
+}
